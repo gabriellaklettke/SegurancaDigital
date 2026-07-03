1632bd8f60701bed86e205777629de02b82f2ee1
gabriellaklettke <gabriella.klettke@escola.pr.gov.br>
2026-07-03 14:38:17 +0000

    aula5

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
@@ -12,10 +15,24 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
-}function aumentaTamanho(){
+    geraSenha();
+}
+
+function aumentaTamanho(){
     if (tamanhoSenha < 20){
        // tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function geraSenha(){
+    let senha = '' ;
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
